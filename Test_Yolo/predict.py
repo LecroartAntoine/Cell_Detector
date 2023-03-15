932808a15f7cbd6d909799438eef253257921d52
LecroartAntoine <115992298+LecroartAntoine@users.noreply.github.com>
2023-03-15 18:34:55 +0100

    update

--- Test_Yolo/predict.py
+++ Test_Yolo/predict.py
@@ -2,18 +2,19 @@ import os, cv2
 from ultralytics import YOLO
 from PIL import Image
 
-
-model = YOLO("C:/Users/antoine.lecroart/Documents/GitHub/Cell_Detector/Test_Yolo/best.pt")
-
-im1 = Image.open("C:/Users/antoine.lecroart/Documents/GitHub/Cell_Detector/Test_Yolo/images/train/IMG_4906.jpg")
-
-model.predict(im1)  # save plotted images
+print(os.getcwd())
 
 
+model = YOLO("Cell_Detector/Test_Yolo/best.pt")
 
-result = Image.open(u'C:/Users/antoine.lecroart/Documents/GitHub/runs/detect/predict/image0.jpg')
+im1 = cv2.imread("Cell_Detector/Test_Yolo/images/train/1678464986902.jpg")
 
 
 
+print(model.predict(im1))
 
-result.show()
+
+# result = Image.open('runs/detect/predict/image0.jpg')
+
+
+# result.show()
