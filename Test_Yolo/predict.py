f6c00815c8f4dc96a704c9d723d95aa7a9cdb4f1
LecroartAntoine <115992298+LecroartAntoine@users.noreply.github.com>
2023-03-15 13:37:29 +0100

    f

--- Test_Yolo/predict.py
+++ Test_Yolo/predict.py
@@ -5,16 +5,15 @@ from PIL import Image
 
 model = YOLO("C:/Users/antoine.lecroart/Documents/GitHub/Cell_Detector/Test_Yolo/best.pt")
 
-im1 = Image.open("C:/Users/antoine.lecroart/Documents/GitHub/Cell_Detector/Test_Yolo/images/train/IMG_4892.jpg")
+im1 = Image.open("C:/Users/antoine.lecroart/Documents/GitHub/Cell_Detector/Test_Yolo/images/train/IMG_4906.jpg")
 
-results = model.predict(source=im1, save=True)  # save plotted images
-
-print(results[0].boxes)
+model.predict(im1)  # save plotted images
 
 
-result = Image.open(u'C:/Users/antoine.lecroart/Documents/GitHub/runs/detect/predict/image0.jpg')
 
-# cv2.rectangle(result,(x_min,y_min),(x_max,y_max),color,2)
+# result = Image.open(u'C:/Users/antoine.lecroart/Documents/GitHub/runs/detect/predict/image0.jpg')
 
 
-result.show()
+
+
+# result.show()
