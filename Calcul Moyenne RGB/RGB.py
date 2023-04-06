08787ab65ef7b673c94b808eeca7305d3f82c7af
LecroartAntoine <115992298+LecroartAntoine@users.noreply.github.com>
2023-04-06 12:14:24 +0200

    Update RGB

--- Calcul Moyenne RGB/RGB.py
+++ Calcul Moyenne RGB/RGB.py
@@ -1,8 +1,16 @@
 import cv2 as cv
 import os
 
+def get_std(liste):
 
-path = 'C:/Users/antoi/Documents/GitHub/Cell_Detector/Calcul Moyenne RGB/Images 28-03/BM'
+    mean = sum(liste) / len(liste)
+    differences = [x - mean for x in liste]
+    squared_differences = [x ** 2 for x in differences]
+    mean_squared_differences = sum(squared_differences) / len(squared_differences)
+    std = mean_squared_differences ** 0.5
+    return(round(std, 2))
+
+path = 'C:/Users/antoi/Documents/GitHub/Cell_Detector/Calcul Moyenne RGB/Images 28-03/Bleu'
 
 im_R, im_G, im_B = [], [], []
 
@@ -14,7 +22,7 @@ for file in os.listdir(path):
     pix_R, pix_G, pix_B = [], [], []
     for row in img:
         for pix in row:
-            if (225 < pix[0]) and (200 < pix[1]):
+            if 230 < pix[0] and 200 < pix[1]:
                 pass
             else:
                 pix_R.append(pix[0])
@@ -25,10 +33,14 @@ for file in os.listdir(path):
         im_G.append(round(sum(pix_G) / len(pix_G)))
         im_B.append(round(sum(pix_B) / len(pix_B)))
     else:
-        print(file)
+
+        print(file[:-6])
         print(f"Rouge : {round(sum(im_R) / len(im_R))}")
+        print(f"Rouge Ecart-Type : {get_std(im_R)}")
         print(f"Vert : {round(sum(im_G) / len(im_G))}")
-        print(f"Bleu : {round(sum(im_B) / len(im_B))}\n\n")
+        print(f"Vert Ecart-Type : {get_std(im_G)}")
+        print(f"Bleu : {round(sum(im_B) / len(im_B))}")
+        print(f"Bleu Ecart-Type : {get_std(im_B)}\n\n")
         im_R, im_G, im_B = [], [], []
     
 
